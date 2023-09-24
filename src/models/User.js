7a26333e88b8be52265af48d92f95327f6c46b4f
Kendy <kendyelisca5@gmail.com>
2023-09-24 16:36:42 -0500

    verification added

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/connection");
+
+const User = sequelize.define("user", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
